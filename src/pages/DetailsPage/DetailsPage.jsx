e11697599c1f89549c6e4356b211eebedc29035e
Yousufjoy <kanekiken333@gmail.com>
2023-10-25 20:06:22 +0600

    need to fix details page

--- src/pages/DetailsPage/DetailsPage.jsx
+++ src/pages/DetailsPage/DetailsPage.jsx
@@ -3,9 +3,14 @@ import { useLoaderData, useParams } from "react-router-dom";
 const DetailsPage = () => {
   const { id } = useParams();
   const data = useLoaderData();
+  const { _id } = data;
 
-  const result = data.find((d) => d._id == id);
-  console.log(result);
+  console.log(data);
+  console.log(`This is matching ${data[0]._id}`);
+  console.log(`this is param ${id}`);
+
+  //   const result = data.find((d) => d._id == id);
+  //   console.log(result);
 
   return <div></div>;
 };
